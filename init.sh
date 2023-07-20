ea3c01cac068dace21ee5871c36743d20f3fb9a3
Runfeng Tsui <runfengtsui@yeah.net>
2023-07-20 18:02:35 +0800

    bash: remove oh-my-fish install file

--- init.sh
+++ init.sh
@@ -53,6 +53,7 @@ ln -s CONFIG_PATH/fish $HOME/.config/fish
 # oh-my-fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
 fish install --path=~/.local/share/omf --config=~/.config/omf
+rm install
 # install Fish Shell theme Bobthefish
 omf install bobthefish
 

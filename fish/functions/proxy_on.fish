a055b7fb4b3ecac67c803955d9774956a02e2409
Runfeng Tsui <runfengtsui@yeah.net>
2023-09-01 20:45:46 +0800

    fish: show port

--- fish/functions/proxy_on.fish
+++ fish/functions/proxy_on.fish
@@ -1,10 +1,10 @@
 # 开启系统代理
 function proxy_on
-    set -gx http_proxy http://127.0.0.1:****
-    set -gx https_proxy http://127.0.0.1:****
+    set -gx http_proxy http://127.0.0.1:7890
+    set -gx https_proxy http://127.0.0.1:7890
     set -gx no_proxy 127.0.0.1 localhost
-    set -gx HTTP_PROXY http://127.0.0.1:****
-    set -gx HTTPS_PROXY http://127.0.0.1:****
+    set -gx HTTP_PROXY http://127.0.0.1:7890
+    set -gx HTTPS_PROXY http://127.0.0.1:7890
     set -gx NO_PROXY 127.0.0.1 localhost
     echo -e "\033[32m[√] 已开启代理\033[0m"
 end

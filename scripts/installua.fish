3ce3a4ca5377827338c72713f60792e98eb82863
Runfeng Tsui <runfengtsui@yeah.net>
2023-11-24 09:47:52 +0800

    fix: set runtime_path to find tar.gz file and delete it

--- scripts/installua.fish
+++ scripts/installua.fish
@@ -1,16 +1,17 @@
 #! /usr/bin/env fish
-
-# download tar install file for Lua
+# Get the path of running the script
+set runtime_path $PWD
+# Download tar.gz file to install Lua
 curl -R -O https://www.lua.org/ftp/lua-5.4.6.tar.gz
-# install denpendences
+# Install denpendency
 sudo apt install libreadline-dev
-# todo: regex to indentify version
+# Todo: regex to indentify version
 if test -f "lua-5.4.6.tar.gz"
     tar zxf lua-5.4.6.tar.gz
     cd lua-5.4.6
     sudo make linux test
     sudo make install
-    rm lua-5.4.6.tar.gz
+    rm $runtime_path/lua-5.4.6.tar.gz
 else
-    echo "No tar file for installing Lua!"
+    echo "No tar.gz file for installing Lua!"
 end

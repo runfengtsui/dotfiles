9a184b07109e2f1dff884346cf3d945f500b5a3c
Runfeng Tsui <runfengtsui@yeah.net>
2023-12-06 15:25:52 +0800

    feat: download tgz file and then install it

--- scripts/installPython39.fish
+++ scripts/installPython39.fish
@@ -1,5 +1,9 @@
 #!/usr/bin/env fish
-if test -e "./Python-3.9.18.tgz"
+# Get the path of running the script
+set runtime_path $PWD
+# Download tgz file to install Python3.9
+curl -R -O https://www.python.org/ftp/python/3.9.18/Python-3.9.18.tgz
+if test -f "Python-3.9.18.tgz"
     # to import tkinter in Python3.9
     sudo apt install tk-dev
     tar -xvf Python-3.9.18.tgz
@@ -11,4 +15,7 @@ if test -e "./Python-3.9.18.tgz"
     sudo cp /opt/Python3/python3.9/lib/libpython3.9.so.1.0 /usr/lib64/
     sudo ln -s /opt/Python3/python3.9/bin/python3.9 /usr/bin/python3.9
     sudo ln -s /opt/Python3/python3.9/bin/pip3.9 /usr/bin/pip3.9
+    rm $runtime_path/Python-3.9.18.tgz
+else
+    echo "No tgz file for install Python3.9"
 end

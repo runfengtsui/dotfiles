8b363143c08a1d942b5a0b4d94122c3fbb45ddd8
Runfeng Tsui <runfengtsui@yeah.net>
2024-01-06 10:07:54 +0800

    feat: disable autopairs for tex files

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,6 +1,9 @@
 return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
-  opts = {},  -- this is equalent ot setup({}) function
+  opts = {
+    -- https://github.com/windwp/nvim-autopairs?tab=readme-ov-file#default-values
+    disable_filetype = { "tex" },
+  },  -- this is equalent ot setup({}) function
 }
 

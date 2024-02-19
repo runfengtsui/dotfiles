6db85d3c9a09cde71a427ac24171a23e3ea41667
Runfeng Tsui <runfengtsui@yeah.net>
2024-02-19 17:37:18 +0800

    polybar: first commit

---
+++ polybar/launch.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+killall -q polybar
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+polybar -c $HOME/dotfiles/polybar/config.ini

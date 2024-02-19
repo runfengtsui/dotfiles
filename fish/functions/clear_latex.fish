f9318ead5bc009cfe7de16ed969cb98303164654
Runfeng Tsui <runfengtsui@yeah.net>
2024-02-19 17:36:32 +0800

    fish: completion for rustup and yazi shell wrapper for fish

--- fish/functions/clear_latex.fish
+++ fish/functions/clear_latex.fish
@@ -1,5 +1,5 @@
 function clear_latex
-    for file in *.aux *.fdb_latexmk *.fls *.log *.synctex.gz *.xdv *.out
+    for file in *.aux *.fdb_latexmk *.fls *.log *.synctex.gz *.xdv *.out *.toc
         if test -f $file
             rm $file
         end

0cdb7e9d9968ccc328757ba4fec1b1e2c5ee587c
Runfeng Tsui <runfengtsui@yeah.net>
2024-02-28 14:40:00 +0800

    install without yarn or npm

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -2,9 +2,8 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
+    build = function()
+      vim.fn["mkdp#util#install"]()
     end,
     ft = { "markdown" },
     config = function()

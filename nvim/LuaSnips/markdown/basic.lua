a7ba35633239df85fe46540e3e13bb7364403b92
Runfeng Tsui <runfengtsui@yeah.net>
2024-04-01 19:44:27 +0800

    code block snippet

---
+++ nvim/LuaSnips/markdown/basic.lua
@@ -0,0 +1,12 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+return {
+  s({ trig = "cb", dscr = "The code block for programming languages." }, {
+    t("```"), i(2, "language"), t({"", ""}),
+    i(1, "Codes"),
+    t({"", "```", ""}),
+  }),
+}

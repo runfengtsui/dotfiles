013b9ec52b8739861e7812059a1ed4550cf6b1b1
Runfeng Tsui <runfengtsui@yeah.net>
2025-01-06 11:13:00 +0800

    update yazi shell wrapper

---
+++ fish/functions/y.fish
@@ -0,0 +1,11 @@
+# using this `y` shell wrapper that provides the ability
+# to change the current working directory
+# https://yazi-rs.github.io/docs/quick-start/#shell-wrapper
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end

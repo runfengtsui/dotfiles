e858554dc6bbb81de159c5bde377a2ab6dd75b6f
Runfeng Tsui <runfengtsui@yeah.net>
2025-03-29 17:01:27 +0800

    update nodejs path, add julia alias and clear more latex middle files

--- fish/config.fish
+++ fish/config.fish
@@ -2,12 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     alias vi="nvim"
     alias vim="nvim"
+    alias julia="julia --threads 8"
     alias dl="aria2c -s16 -x16 -k1M"
     # path of xmake and poetry
     set -x PATH $HOME/.local/bin $PATH
     # path of Julia and juliaup
     set -x PATH $HOME/.juliaup/bin $PATH
     # path of nodejs and npm
-    set -x PATH $HOME/.local/opt/nodejs/aliases/default/bin $PATH
+    set -x PATH $HOME/.local/share/fnm/aliases/default/bin $PATH
     set -x EDITOR nvim
 end
--- fish/functions/clear_latex.fish
+++ fish/functions/clear_latex.fish
@@ -1,5 +1,5 @@
 function clear_latex
-    for file in *.aux *.fdb_latexmk *.fls *.log *.synctex.gz *.xdv *.out *.toc
+    for file in *.aux *.fdb_latexmk *.fls *.log *.synctex.gz *.xdv *.out *.toc *.bbl *.blg *.nav *.snm # '*.synctex(busy)'
         if test -f $file
             rm $file
         end

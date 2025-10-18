051c363c2d6b97d2fc472125c151002e534115ab
Runfeng Tsui <runfengtsui@yeah.net>
2025-10-18 17:17:19 +0800

    feat: script to download gh binary file on Linux

---
+++ scripts/gh.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import sys
+import os
+import tempfile
+import urllib.request, urllib.error
+import tarfile
+
+def reporthook(block_num, block_size, total_size):
+    downloaded = block_num * block_size
+    progress = downloaded / total_size * 100
+    sys.stdout.write(f"Downloading Prograes: {progress:.2f}%")
+    sys.stdout.flush()
+
+if os.uname().machine == "x86_64":
+    ARCH = "amd64"
+elif os.uname().machine == "aarch64":
+    ARCH = "arm64"
+
+VERSION = "2.82.0"
+url = f"https://github.com/cli/cli/releases/download/v{VERSION}/gh_{VERSION}_linux_{ARCH}.tar.gz"
+filename = os.path.basename(url)
+
+with tempfile.TemporaryDirectory() as temp_dir:
+    try:
+        print(f"Downloading {filename} ...")
+        filename = os.path.join(temp_dir, filename)
+        urllib.request.urlretrieve(url, filename, reporthook)
+        print("\nDownloaded Successfully!")
+
+        print("Extracting to /opt ...")
+        with tarfile.open(filename, "r:gz") as tar_file:
+            tar_file.extractall("/opt/", filter="data")
+
+        print("Creating symbolic link ...")
+        julia_bin_path = f"/opt/gh_{VERSION}_linux_{ARCH}/bin/gh"
+        os.symlink(julia_bin_path, "/usr/local/bin/gh")
+
+        print("gh installation completed successfully!")
+
+    except urllib.error.URLError:
+        print("\nDownload Error! Please check your network connection and try again.")
+
+    except PermissionError:
+        print("Permission denied. This script requires sudo privileges for some operations.")
+        print("Please run with sudo.")
+
+    except Exception as e:
+        print(f"\nAn error occured: {e}")
+
